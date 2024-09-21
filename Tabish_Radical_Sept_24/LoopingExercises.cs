0c4d74e039ce0753967fe952d729abc417bf6a08
Tabish Ahmad <tabish0211@hotmail.com>
2024-09-21 12:29:57 +0530

    Looping exercises

---
+++ Tabish_Radical_Sept_24/LoopingExercises.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabish_Radical_Sept_24
+{
+    internal class LoopingExercises
+    {
+        static void Main()
+        {
+            // SumOfNnaturalNumbers();
+            // Tables();
+            //PrintOddNumbersFromRange();
+
+            //BreakTheLoop();
+            Factorial();
+
+
+            Console.ReadLine();
+        }
+
+        //static methods only call static method
+        static void SumOfNnaturalNumbers()
+        {
+
+            //WAP to display sum of  n natural numbers          
+
+            Console.WriteLine("Tell me number of digits");
+
+            int digitsCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= digitsCount; i++)
+            {
+                sum = sum + num % 10;//sum=0,3---sum=3--1st----sum=3,2--sum=5---2nd---sum=5,1=sum=6
+                num = num / 10;//num=12---1st---num=1--2nd---num=0;
+            }
+
+            Console.WriteLine($"Sum={sum}");
+        }
+
+        static void Tables()
+        {
+
+            //Table of 2
+            //2*1=2 2*2=4----------2*10=20
+            //
+
+            //Table of 5
+
+
+
+            //WAP to print table 
+            //user will input the start value and end value of the table 
+            //e.g 3---6---3,4,5,6
+
+            //3*1=3
+            //3*2=6
+            //......
+
+            //4*1=4
+            //4*2=8...10
+
+            //5*1=
+
+            //6*1=6
+
+            //---Range---3---6
+            //---strat*i--1---10
+            Console.WriteLine("Enter the starting value");
+            int initial = Convert.ToInt32(Console.ReadLine());//2
+
+
+            Console.WriteLine("Enter the final value");
+            int final = Convert.ToInt32(Console.ReadLine());//5
+
+            for (int i = 1; i < 11; i++)//1---10--i---2
+            {
+                for (int start = initial; start <= final; start++)//start-2--2<=5---start-3---start=5---start--6
+                    //2*1=2  ----5*1=5
+                    //-
+                {
+
+                    Console.Write($"{start}*{i}={start * i} \t");
+                }
+                Console.WriteLine();
+                
+            }
+            
+            //for (int start = initial; start <=final; start++)
+            //{
+            //    Console.WriteLine($"*********Table of {start}");
+            //    for (int i = 1; i < 11; i++)
+            //    {
+            //        Console.WriteLine($"{start}*{i}={start * i}");
+            //    }
+
+
+                
+
+            //    //2*1=2   3*1 4*1=4   5*1=5
+            //    //2*2=4
+            //    //
+            //    //
+            //    //
+            //}
+            
+        
+            
+        
+        }
+
+        static void PrintOddNumbersFromRange()
+        {
+            Console.WriteLine("Enter the starting value");
+            int initial = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the final value");
+            int final = Convert.ToInt32(Console.ReadLine());
+
+            //11----20---11,13,15,17....
+
+            for (int i = initial; i <= final; i++)
+            {
+                if (i%2==0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(i);//1,2,3,4,5,6,7,8,9,10
+                //Even number series comma separated -==10--20====10,12,14,16,18,20
+
+             
+            }
+
+
+
+        }
+
+        static void BreakTheLoop()
+        {
+
+            for (int i = 1; i < 11; i++)
+            {
+                if (i==6)
+                {
+                    break;
+                }
+                Console.WriteLine(i);//in a geiven series if you find i--6 --come out of the loop
+
+            }
+
+            Console.WriteLine("Loop is break force fully");
+        }
+
+        static void Factorial()
+        {
+
+            Console.WriteLine( "Enetr the factorial value");
+            int factVal = Convert.ToInt32(Console.ReadLine());
+            int factOutput = 1;
+            for (int i = factVal; i >0; i--)
+            {
+                factOutput=factOutput*i;
+            }
+            Console.WriteLine($"factorial of {factVal} is {factOutput}");
+        }
+
+        //Take the range value from user ---2---8 print factorial of 2 till 8
+
+        
+    }
+}
--- Tabish_Radical_Sept_24/Program.cs
+++ Tabish_Radical_Sept_24/Program.cs
@@ -1,252 +1,252 @@
-﻿using System.Threading.Channels;
+﻿//using System.Threading.Channels;
 
-namespace Tabish_Radical_Sept_24
-{
+//namespace Tabish_Radical_Sept_24
+//{
 
-    //\n--new line 
-    internal class Program
-    {
-        //csc.exe--compiler 
-        //entry point method
-        static void Main()
-        {
-            //    //char --2 byte
-            //    string message = "Hello";//derived datatypes----reference datatype---heap---runtime 
-            //    char ch = 'a';//2-bytes
-            //    int x = 1;//4-bytes
-            //    float f = 2.5F;//4 bytes//primitive datatype
-            //    double d = 1.00005D;//8 bytes
-            //    long l = 16L;//8-bytes
+//    //\n--new line 
+//    internal class Program
+//    {
+//        //csc.exe--compiler 
+//        //entry point method
+//        static void Main()
+//        {
+//            //    //char --2 byte
+//            //    string message = "Hello";//derived datatypes----reference datatype---heap---runtime 
+//            //    char ch = 'a';//2-bytes
+//            //    int x = 1;//4-bytes
+//            //    float f = 2.5F;//4 bytes//primitive datatype
+//            //    double d = 1.00005D;//8 bytes
+//            //    long l = 16L;//8-bytes
 
-            //char, float,int,long,double they are known as value type --stack--compile time
-
-
-
-            //memory location---you can store any value depending datatype
-
-            // Console.WriteLine("Enter your name and salary");//hello, world!
-            // //input 
-            // string name = Console.ReadLine();
-            // int sal = Convert.ToInt32(Console.ReadLine());
-            // //Console.WriteLine("name="+name);
-            // //Console.WriteLine($"name={name}");
-            //// Console.WriteLine("Tell me your salary");
-
-            //  Console.WriteLine($"name= {name} salary ={sal}");
-            //Console.Write("hello tabish\n");// hello tabish
-            //
-
-            //enter-bs,hra-10%,ta-20%--gross salary
-            //Console.WriteLine("Enter basic salary");
-            //int bs = Convert.ToInt32(Console.ReadLine());
-
-            //double hra = 0.1 * bs;
-            //double ta = 0.2 * bs;
-            //double gs = bs + hra + ta;
-            //Console.WriteLine($"Your basic salary= {bs} and gross salary= {gs}");
-
-            //a=1,b=2;
-            //b=1,a=2;
-            //temp---a,,a--b,b--temp
-            //int a = 1, b = 2;
-            //Console.WriteLine($"Before swapping a={a} and b={b}");
-            ////int temp;
-            ////temp = a;
-            ////a= b;
-            ////b= temp;
-            //a = a + b;//3
-            //b = a - b;//3-2==1
-            //a = a - b;//3-1=2
-
-            //Console.WriteLine($"After swapping a={a} and b={b}");
-
-            //Decision making controls
-            //int x = 1, y = 2;
-            ////bool flag = true;
-            ////flag = false;
-            ////flag=x > y;
-            ////flag = x > y && y < 1;
-            ////Console.WriteLine( flag);
-
-            //if (x>y)
-            //{
-            //    Console.WriteLine("Yes y is greatert than x and it is a true statement");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("No x is smaller than yand it is a false statement");
-            //}
-
-            ////nested if-else
-            //if (true)
-            //{
-            //    if (true)
-            //    {
-
-            //    }
-            //    else
-            //    {
-
-            //    }
+//            //char, float,int,long,double they are known as value type --stack--compile time
 
 
-            //}
 
-            //else { 
+//            //memory location---you can store any value depending datatype
 
-            //    if (true) { }
-            //    else { }
+//            // Console.WriteLine("Enter your name and salary");//hello, world!
+//            // //input 
+//            // string name = Console.ReadLine();
+//            // int sal = Convert.ToInt32(Console.ReadLine());
+//            // //Console.WriteLine("name="+name);
+//            // //Console.WriteLine($"name={name}");
+//            //// Console.WriteLine("Tell me your salary");
 
-            //}
+//            //  Console.WriteLine($"name= {name} salary ={sal}");
+//            //Console.Write("hello tabish\n");// hello tabish
+//            //
+
+//            //enter-bs,hra-10%,ta-20%--gross salary
+//            //Console.WriteLine("Enter basic salary");
+//            //int bs = Convert.ToInt32(Console.ReadLine());
+
+//            //double hra = 0.1 * bs;
+//            //double ta = 0.2 * bs;
+//            //double gs = bs + hra + ta;
+//            //Console.WriteLine($"Your basic salary= {bs} and gross salary= {gs}");
+
+//            //a=1,b=2;
+//            //b=1,a=2;
+//            //temp---a,,a--b,b--temp
+//            //int a = 1, b = 2;
+//            //Console.WriteLine($"Before swapping a={a} and b={b}");
+//            ////int temp;
+//            ////temp = a;
+//            ////a= b;
+//            ////b= temp;
+//            //a = a + b;//3
+//            //b = a - b;//3-2==1
+//            //a = a - b;//3-1=2
+
+//            //Console.WriteLine($"After swapping a={a} and b={b}");
+
+//            //Decision making controls
+//            //int x = 1, y = 2;
+//            ////bool flag = true;
+//            ////flag = false;
+//            ////flag=x > y;
+//            ////flag = x > y && y < 1;
+//            ////Console.WriteLine( flag);
+
+//            //if (x>y)
+//            //{
+//            //    Console.WriteLine("Yes y is greatert than x and it is a true statement");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("No x is smaller than yand it is a false statement");
+//            //}
+
+//            ////nested if-else
+//            //if (true)
+//            //{
+//            //    if (true)
+//            //    {
+
+//            //    }
+//            //    else
+//            //    {
+
+//            //    }
 
 
-            ////ladder if -else
-            //if (true)
-            //{
+//            //}
 
-            //}
-            //else if (true)
-            //{
-            //}
-            //else { 
+//            //else { 
 
-            //}
+//            //    if (true) { }
+//            //    else { }
 
-            //int choice = 1;
-            //switch (choice)
-            //{
-            //    case 1:
-            //        //work
-            //        break;
-            //    case 2:
-            //        //work
-            //        break;
-            //    case 3:
-            //        //work
-            //        break;
-            //    default:
-            //        break;
-            //}
-
-            //string str=x > y ?"Hello world" :"xyz";
-            //Console.WriteLine(  str);
+//            //}
 
 
-            //sum of 3 digit number---234=2+3+4=9
-            //Console.WriteLine( "enter three digit number");
-            //int num=Convert.ToInt32( Console.ReadLine() );
-            //int oldValue = num;
-            ////234---4,3,2---4+3+2----print summation
-            //int a = num % 10;//4
-            //num = num / 10;//23
-            //int b = num % 10;//3
-            //num = num / 10;//2
+//            ////ladder if -else
+//            //if (true)
+//            //{
 
-            //int sum = a + b + num;
-            //Console.WriteLine(  sum);
-            //reverse the three digit---234----432---x*100+y*10+z*1
-            //int rev = a * 100 + b * 10 + num;
-            //Console.WriteLine(  rev);
+//            //}
+//            //else if (true)
+//            //{
+//            //}
+//            //else { 
 
-            //if (oldValue==rev)
-            //{
-            //    Console.WriteLine($"old num={oldValue} and Reverse {rev} are same");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"old num={oldValue} and Reverse {rev} are not  same");
-            //}
+//            //}
 
-            //string message = oldValue == rev ? $"old num={oldValue} and Reverse {rev} are same" : $"old num={oldValue} and Reverse {rev} are not  same";
-            //Console.WriteLine( message);
+//            //int choice = 1;
+//            //switch (choice)
+//            //{
+//            //    case 1:
+//            //        //work
+//            //        break;
+//            //    case 2:
+//            //        //work
+//            //        break;
+//            //    case 3:
+//            //        //work
+//            //        break;
+//            //    default:
+//            //        break;
+//            //}
 
-            //Who is the youngest one amongst three brothers
+//            //string str=x > y ?"Hello world" :"xyz";
+//            //Console.WriteLine(  str);
 
-            Console.WriteLine("Enter the age of Ramesh");
-            int rameshAge=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the age of Suresh");
-            int sureshAge = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the age of kamesh");
-            int kameshAge = Convert.ToInt32(Console.ReadLine());
 
-            if (rameshAge < sureshAge)
-            {
-                if (rameshAge < kameshAge)
-                {
-                    Console.WriteLine($"Ramesh is youngest one and his age is {rameshAge}");
-                }
-                else
-                {
-                    Console.WriteLine($"Kamesh is youngest one and his age is {kameshAge}");
-                }
-            }
-            else {
+//            //sum of 3 digit number---234=2+3+4=9
+//            //Console.WriteLine( "enter three digit number");
+//            //int num=Convert.ToInt32( Console.ReadLine() );
+//            //int oldValue = num;
+//            ////234---4,3,2---4+3+2----print summation
+//            //int a = num % 10;//4
+//            //num = num / 10;//23
+//            //int b = num % 10;//3
+//            //num = num / 10;//2
 
-                if (sureshAge < kameshAge)
-                {
-                    Console.WriteLine($"Suresh is youngest one and his age is {sureshAge}");
-                }
-                else
-                {
-                    Console.WriteLine($"Kamesh is youngest one and his age is {kameshAge}");
-                }
+//            //int sum = a + b + num;
+//            //Console.WriteLine(  sum);
+//            //reverse the three digit---234----432---x*100+y*10+z*1
+//            //int rev = a * 100 + b * 10 + num;
+//            //Console.WriteLine(  rev);
 
-            }
+//            //if (oldValue==rev)
+//            //{
+//            //    Console.WriteLine($"old num={oldValue} and Reverse {rev} are same");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine($"old num={oldValue} and Reverse {rev} are not  same");
+//            //}
 
-            //Loop--repetative task
+//            //string message = oldValue == rev ? $"old num={oldValue} and Reverse {rev} are same" : $"old num={oldValue} and Reverse {rev} are not  same";
+//            //Console.WriteLine( message);
+
+//            //Who is the youngest one amongst three brothers
+
+//            Console.WriteLine("Enter the age of Ramesh");
+//            int rameshAge=Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter the age of Suresh");
+//            int sureshAge = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter the age of kamesh");
+//            int kameshAge = Convert.ToInt32(Console.ReadLine());
+
+//            if (rameshAge < sureshAge)
+//            {
+//                if (rameshAge < kameshAge)
+//                {
+//                    Console.WriteLine($"Ramesh is youngest one and his age is {rameshAge}");
+//                }
+//                else
+//                {
+//                    Console.WriteLine($"Kamesh is youngest one and his age is {kameshAge}");
+//                }
+//            }
+//            else {
+
+//                if (sureshAge < kameshAge)
+//                {
+//                    Console.WriteLine($"Suresh is youngest one and his age is {sureshAge}");
+//                }
+//                else
+//                {
+//                    Console.WriteLine($"Kamesh is youngest one and his age is {kameshAge}");
+//                }
+
+//            }
+
+//            //Loop--repetative task
             
-            for (int i = 1; i < 11; i++)
-            {
-                Console.WriteLine(2*i);
-            }
+//            for (int i = 1; i < 11; i++)
+//            {
+//                Console.WriteLine(2*i);
+//            }
 
             
-            int j = 1;
-            while (j < 11)
-            {
-                Console.WriteLine(2 * j);
-                j++;
-            }
+//            int j = 1;
+//            while (j < 11)
+//            {
+//                Console.WriteLine(2 * j);
+//                j++;
+//            }
 
-            int k = 1;
-            do
-            {
-                Console.WriteLine(2 * k);
-                k++;
+//            int k = 1;
+//            do
+//            {
+//                Console.WriteLine(2 * k);
+//                k++;
 
-            } while (k<11);
+//            } while (k<11);
 
-            char ch = 'Y';
-            do
-            {
-                Console.WriteLine("please enter number for which you wish to print the tables");
-                int num = Convert.ToInt32(Console.ReadLine());
-                for (int i = 1; i < 11; i++)
-                {
-                    Console.WriteLine(num * i);
-                }
+//            char ch = 'Y';
+//            do
+//            {
+//                Console.WriteLine("please enter number for which you wish to print the tables");
+//                int num = Convert.ToInt32(Console.ReadLine());
+//                for (int i = 1; i < 11; i++)
+//                {
+//                    Console.WriteLine(num * i);
+//                }
 
-                Console.WriteLine("Do you want to print another number table ");
-                ch= Convert.ToChar(Console.ReadLine());
-            } while (ch=='y' || ch=='Y');
+//                Console.WriteLine("Do you want to print another number table ");
+//                ch= Convert.ToChar(Console.ReadLine());
+//            } while (ch=='y' || ch=='Y');
            
 
             
 
-            //foreach (var item in collection)
-            //{
+//            //foreach (var item in collection)
+//            //{
 
-            //}
+//            //}
 
-            Console.ReadLine();
-        }
+//            Console.ReadLine();
+//        }
 
         
-    }
+//    }
 
 
     
 
    
 
-}
+//}
