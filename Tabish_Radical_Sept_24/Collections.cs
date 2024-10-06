fd7a6cdb4a5bb884e353355669f9a70d007f23fc
Tabish Ahmad <tabish0211@hotmail.com>
2024-10-06 12:54:27 +0530

    OOP

--- Tabish_Radical_Sept_24/Collections.cs
+++ Tabish_Radical_Sept_24/Collections.cs
@@ -1,143 +1,145 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Tabish_Radical_Sept_24
-{
-    internal class Collections
-    {
-        static void Main()
-        {
-            ////    string wish = "y";
-            ////   // string[] strCity = new string[3];//size
-            ////    List<string> list = new List<string>();//Dhiraj
-            ////    while (wish.ToLower().Equals("y"))//i dont know the size of my datastructure
-            ////    {
-            ////        Console.WriteLine("Enter the City");
-            ////        string city = Console.ReadLine();///Pune---Patna
-            ////        //strCity[0] = wish;
-            ////        list.Add(city);
-
-
-            ////        Console.WriteLine("Do you want to add more city");
-            ////        wish = Console.ReadLine();//yes Yes YES YeS
-            ////    }
-            ////Generic |Non generic
-            ////Generic--Where we have to define what dadtype should be store in my collection
-            ////List<string>
-
-            ////print all the city
+//namespace Tabish_Radical_Sept_24
+//{
+//    internal class Collections
+//    {
+//        static void Main()
+//        {
+//            ////    string wish = "y";
+//            ////   // string[] strCity = new string[3];//size
+//            ////    List<string> list = new List<string>();//Dhiraj
+//            ////    while (wish.ToLower().Equals("y"))//i dont know the size of my datastructure
+//            ////    {
+//            ////        Console.WriteLine("Enter the City");
+//            ////        string city = Console.ReadLine();///Pune---Patna
+//            ////        //strCity[0] = wish;
+//            ////        list.Add(city);
 
 
-            ////Console.WriteLine("City Details:");
-            ////foreach (string s in list)
-            ////{
-            ////    Console.WriteLine(s);
+//            ////        Console.WriteLine("Do you want to add more city");
+//            ////        wish = Console.ReadLine();//yes Yes YES YeS
+//            ////    }
+//            ////Generic |Non generic
+//            ////Generic--Where we have to define what dadtype should be store in my collection
+//            ////List<string>
 
-            ////}
-            //List<string> Dhiraj_list = new List<string> { "Bombay", "Kolkata" };
-            //List<string> tabish_list = new List<string>{ "Allahabad", "New Delhi", "Bombay" };
-            //List<string> aggreagtedList = new List<string>{ "Chennai", "Nashik" };
-            //aggreagtedList.AddRange(Dhiraj_list);
-            //aggreagtedList.AddRange(tabish_list);
+//            ////print all the city
 
 
+//            ////Console.WriteLine("City Details:");
+//            ////foreach (string s in list)
+//            ////{
+//            ////    Console.WriteLine(s);
 
-
-            //////list.Remove("Pune");
-
-            ////Console.WriteLine("\n\nCity Details after some operations:");
-            ////foreach (string s in aggreagtedList)
-            ////{
-            ////    Console.WriteLine(s);
-
-            ////}
-            //////Remove duplicacy
-            ////List<string> nonDuplicate = aggreagtedList.Distinct().ToList();
-            ////Console.WriteLine("\n\nCity Details after some operations:");
-            ////foreach (string s in nonDuplicate)
-            ////{
-            ////    Console.WriteLine(s);
-
-            ////}
+//            ////}
+//            //List<string> Dhiraj_list = new List<string> { "Bombay", "Kolkata" };
+//            //List<string> tabish_list = new List<string>{ "Allahabad", "New Delhi", "Bombay" };
+//            //List<string> aggreagtedList = new List<string>{ "Chennai", "Nashik" };
+//            //aggreagtedList.AddRange(Dhiraj_list);
+//            //aggreagtedList.AddRange(tabish_list);
 
 
 
 
-            ////aggreagtedList.RemoveRange(4, 2);
-            ////Console.WriteLine("\n\nCity Details after some operations:");
-            ////foreach (string s in aggreagtedList)
-            ////{
-            ////    Console.WriteLine(s);
+//            //////list.Remove("Pune");
 
-            ////}
-            ////aggreagtedList.ForEach(s => Console.WriteLine(s));//PLINQ---Core of CPU--Dual core
-            ////if (aggreagtedList.Contains("P"))
-            ////{
+//            ////Console.WriteLine("\n\nCity Details after some operations:");
+//            ////foreach (string s in aggreagtedList)
+//            ////{
+//            ////    Console.WriteLine(s);
 
-            ////}
+//            ////}
+//            //////Remove duplicacy
+//            ////List<string> nonDuplicate = aggreagtedList.Distinct().ToList();
+//            ////Console.WriteLine("\n\nCity Details after some operations:");
+//            ////foreach (string s in nonDuplicate)
+//            ////{
+//            ////    Console.WriteLine(s);
+
+//            ////}
 
 
 
-            ////string city_name = list.ElementAt(0);
-            ////list.Remove(city_name);
-            ////int noOFItem = list.Count();
-            ////if (list.Count()>3)
-            ////{
-            ////    //below block
-            ////}
 
-            ////if (list.Any())
-            ////{
-            ////    //below block
-            ////}
+//            ////aggreagtedList.RemoveRange(4, 2);
+//            ////Console.WriteLine("\n\nCity Details after some operations:");
+//            ////foreach (string s in aggreagtedList)
+//            ////{
+//            ////    Console.WriteLine(s);
 
-            ////Non generic collection
-            //ArrayList arrayList=new ArrayList();
-            //arrayList.Add("Ramesh");
-            //arrayList.Add(10000);
-            //arrayList.Add('E');
-            //arrayList.Add(aggreagtedList);
+//            ////}
+//            ////aggreagtedList.ForEach(s => Console.WriteLine(s));//PLINQ---Core of CPU--Dual core
+//            ////if (aggreagtedList.Contains("P"))
+//            ////{
 
-            //foreach (var item in arrayList)
-            //{
-            //    Console.WriteLine( item);
-            //}
+//            ////}
 
-            //var Dhiraj_list1 = new List<string> { "Bombay", "Kolkata" };
 
-            ////var x = 1;
-            ////x = "Rakesh";
 
-            //dynamic y = 1;
-            //y = "Tabish";
-            //Console.WriteLine( y);
+//            ////string city_name = list.ElementAt(0);
+//            ////list.Remove(city_name);
+//            ////int noOFItem = list.Count();
+//            ////if (list.Count()>3)
+//            ////{
+//            ////    //below block
+//            ////}
 
-            int x = 1, y = 2;
-            int s;
-            ModifyValue(ref x,ref y);
-            //Console.WriteLine($"x={x} and y={y}");
-            Sum(x, y,out s);
-            Console.WriteLine($"sum={s}");
-            Console.ReadLine();
-        }
+//            ////if (list.Any())
+//            ////{
+//            ////    //below block
+//            ////}
 
-        static void ModifyValue(ref int x,ref int y)//formal argument
-        {
-            x = x + 2;
-            y= y + 2;
-            Console.WriteLine($"x={x} and y={y}");
+//            ////Non generic collection
+//            //ArrayList arrayList=new ArrayList();
+//            //arrayList.Add("Ramesh");
+//            //arrayList.Add(10000);
+//            //arrayList.Add('E');
+//            //arrayList.Add(aggreagtedList);
 
-        }
+//            //foreach (var item in arrayList)
+//            //{
+//            //    Console.WriteLine( item);
+//            //}
 
-        static void Sum( int x,  int y,out int sum)//formal argument
-        {
-             sum = x + y;
+//            //var Dhiraj_list1 = new List<string> { "Bombay", "Kolkata" };
+
+//            ////var x = 1;
+//            ////x = "Rakesh";
+
+//            //dynamic y = 1;
+//            //y = "Tabish";
+//            //Console.WriteLine( y);
+
+//            int x = 1, y = 2;
+//            int s;
+//            ModifyValue(ref x,ref y);
+//            //Console.WriteLine($"x={x} and y={y}");
+//            Sum(x, y,out s);
+//            Console.WriteLine($"sum={s}");
+//            Console.ReadLine();
+//        }
+
+//        static void ModifyValue(ref int x,ref int y)//formal argument
+//        {
+//            x = x + 2;
+//            y= y + 2;
+//            Console.WriteLine($"x={x} and y={y}");
+
+//        }
+
+//        static void Sum( int x,  int y,out int sum)//formal argument
+//        {
+//             sum = x + y;
           
 
-        }
-    }
-}
+//        }
+
+       
+//    }
+//}
---
+++ Tabish_Radical_Sept_24/OOP/ClassObject.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabish_Radical_Sept_24.OOP
+{
+
+    class Launcher
+    {
+        //static void Main()
+        //{
+
+        //    //(new ClassObject()).Display();
+        //    //ClassObject obj = new ClassObject();
+        //    //obj.Display();
+
+        //    //(new ClassObject()).Display();
+        //    //(new ClassObject()).Operation();
+
+        //    Console.ReadLine();
+
+        //}
+
+
+
+
+
+
+        static void Main()
+        {
+            Customer customer = new Customer();
+
+            //customer.SetCustomerId(1);
+            //customer.SetCustomerName("Ramesh");
+            //var id=customer.GetCustomerId();
+            //var name=customer.GetCustomerName();
+
+            customer.ID = 1;
+            customer.Name = "Ramesh";
+            
+
+            Console.WriteLine($"Id={customer.ID} and name={customer.Name}");
+            Console.ReadLine();
+
+        }
+    }
+
+    //encapsulation--Implement properties
+
+    class Customer
+    {
+
+        //private int _id;
+        //private string _name;
+
+        ////C# 2.0
+        //public int ID {
+        //    set { 
+
+        //        _id = value;
+
+        //    }
+        //    get
+        //    {
+        //        return _id;
+        //    }        
+
+
+        //}
+
+        //public string Name
+        //{
+        //    set
+        //    {
+
+        //        _name = value;
+
+        //    }
+        //    get
+        //    {
+        //        return _name;
+        //    }
+
+
+        //}
+
+        public int ID { get; set; }
+        public string Name  { get; set; }
+
+        ////Defauult attribute is private for methods in C#
+        //public void SetCustomerId(int cid)
+        //{
+
+        //    id = cid;
+
+        //}
+
+        //public void SetCustomerName(string cname)
+        //{
+
+        //    name = cname;
+
+        //}
+
+        //public int GetCustomerId()
+        //{
+
+        //    return id;
+
+        //}
+
+        //public string GetCustomerName()
+        //{
+
+        //    return name;
+
+        //}
+
+
+    }
+
+
+
+
+
+
+
+
+
+    class ClassObject
+    {
+        int x;
+        public void Operation()
+        {
+
+            x = x + 1;
+        }
+
+        public void Display()
+        {
+
+
+            Console.WriteLine(x);
+        }
+
+
+    }
+
+    
+}
