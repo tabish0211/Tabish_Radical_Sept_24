95391b8e6c26212fb115c22f3d11f7f6f3b6e108
Tabish Ahmad <tabish0211@hotmail.com>
2025-01-31 23:26:27 +0530

    SOLID

---
+++ Tabish_Radical_Sept_24/GoodCode.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Tabish_Radical_Sept_24
+{
+    interface IColor
+    {
+        string Color { get; set; }
+
+    }
+    public interface IProduct
+    {
+        string Name { get; set; }
+        double Price { get; set; }
+        string Category { get; set; }
+        double GetdiscountedPrice();
+        void OrderGeneartir(IProduct product);
+
+    }
+    class Elctronics : IProduct, IColor
+    {
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+
+        private readonly IOrderProcessorOps _orderProcessorOps;
+        public Elctronics(IOrderProcessorOps orderProcessorOps)
+        {
+            _orderProcessorOps = orderProcessorOps;
+        }
+        public double GetdiscountedPrice()
+        {
+            return Price * 0.9;
+
+        }
+
+        public void OrderGeneartir(IProduct product)
+        {
+           // OrderProcessorOps processorOps = new OrderProcessorOps(new DatabaseOps(), new EmailCommunicationOps());
+            _orderProcessorOps.ProcessOrder(product);
+        }
+    }
+
+    class Clothing : IProduct, IColor
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+
+        private readonly IOrderProcessorOps _orderProcessorOps;
+        public Clothing(IOrderProcessorOps orderProcessorOps)
+        {
+            _orderProcessorOps = orderProcessorOps;
+        }
+        public double GetdiscountedPrice()
+        {
+            return Price * 0.8;
+
+        }
+
+        public void OrderGeneartir(IProduct product)
+        {
+           // OrderProcessorOps processorOps = new OrderProcessorOps(new DatabaseOps(), new EmailCommunicationOps());
+            _orderProcessorOps.ProcessOrder(product);
+        }
+    }
+
+
+    class SIMCard : IProduct
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+
+        private readonly IOrderProcessorOps _orderProcessorOps;
+        public SIMCard(IOrderProcessorOps orderProcessorOps)
+        {
+            _orderProcessorOps = orderProcessorOps;
+        }
+        public double GetdiscountedPrice()
+        {
+            return Price * 0.7;
+
+        }
+
+        public void OrderGeneartir(IProduct product)
+        {
+            //OrderProcessorOps processorOps = new OrderProcessorOps(new DatabaseOps(), new EmailCommunicationOps());
+            _orderProcessorOps.ProcessOrder(product);
+        }
+    }
+
+    class Others : IProduct
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+
+        private readonly IOrderProcessorOps _orderProcessorOps;
+        public Others(IOrderProcessorOps orderProcessorOps)
+        {
+            _orderProcessorOps = orderProcessorOps;
+        }
+        public double GetdiscountedPrice()
+        {
+            return Price;
+        }
+
+        public void OrderGeneartir(IProduct product)
+        {
+           // OrderProcessorOps processorOps = new OrderProcessorOps(new DatabaseOps(),new EmailCommunicationOps());
+            _orderProcessorOps.ProcessOrder(product);
+        }
+    }
+
+    interface IOrderProcessorOps
+    {
+        void ProcessOrder(IProduct product);
+    }
+    class OrderProcessorOps:IOrderProcessorOps
+    {
+        private readonly IDatabaseOps _database;
+        private readonly ICommunication _comm;
+        public OrderProcessorOps(IDatabaseOps database, ICommunication comm)
+        {
+            _database = database;
+            _comm = comm;
+        }
+        public void ProcessOrder(IProduct product)
+        {
+            Console.WriteLine("processing order for: " + product.Name);
+            _database.SavetoDatabase(product);
+            _comm.SendemailNotification(product);
+        }
+
+    }
+
+    interface IDatabaseOps
+    {
+        void SavetoDatabase(IProduct product);
+    }
+    class DatabaseOps : IDatabaseOps
+    {
+        public void SavetoDatabase(IProduct product)
+        {
+            Console.WriteLine("saving product to database...");
+        }
+
+    }
+
+    interface ICommunication
+    {
+        void SendemailNotification(IProduct product);
+    }
+    class EmailCommunicationOps: ICommunication
+    {
+        public void SendemailNotification(IProduct product)
+        {
+            Console.WriteLine("sending email notification for: " + product.Name);
+        }
+
+    }
+
+
+
+
+    internal class GoodCode
+    {
+        static void Main()
+        {
+            IProduct electronic = new Elctronics(
+                new OrderProcessorOps(
+                    new DatabaseOps(),
+                    new EmailCommunicationOps())
+                )
+            {
+
+                Name = "Samsung",
+                Price = 10000,
+                Category = "ELectronics",
+                Color = "Red"
+            };
+
+            IProduct cloth = new Clothing(
+                new OrderProcessorOps(
+                    new DatabaseOps(),
+                    new EmailCommunicationOps())
+                )
+            {
+
+                Name = "Jeans",
+                Price = 1000,
+                Category = "Fabric",
+                Color = "Blue"
+            };
+
+            IProduct simcard = new SIMCard(
+                new OrderProcessorOps(
+                    new DatabaseOps(),
+                    new EmailCommunicationOps())
+                )
+            {
+
+                Name = "airtel",
+                Price = 800,
+                Category = "Services",
+                //Color = "Red"//SIM card dont have 
+            };
+
+            electronic.OrderGeneartir(electronic);
+            cloth.OrderGeneartir(cloth);
+            simcard.OrderGeneartir(simcard);
+
+
+        }
+    }
+}
---
+++ Tabish_Radical_Sept_24/SolidImplementation.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabish_Radical_Sept_24
+{
+    interface IProductColor
+    {
+        public string Color { get; set; } //LSP
+    }
+
+    interface IProduct {
+        string Name { get; set; }
+        double Price { get; set; }
+        string Category { get; set; }
+        double GetdiscountedPrice();
+
+
+            }
+    public abstract class S_Product:IProduct
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public virtual double GetdiscountedPrice() { 
+        
+            return Price;
+
+        }
+    }
+    class Electronics : IProduct,IProductColor//ISP
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+
+        public  double GetdiscountedPrice()
+        {
+            return Price * 0.9;
+        }
+
+        public void ElectrnicProcessor(IProduct product)
+        {
+            OrderProcessor orderProcessor = new OrderProcessor();
+            orderProcessor.ProcessOrder(product);
+        }
+    }
+    class Clothing : IProduct,IProductColor
+    {
+        public string Color { get; set; }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Category { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public  double GetdiscountedPrice()
+        {
+            return Price * 0.8;
+        }
+
+    }
+
+
+    //SHould not have color --violation of 
+    class SIM : IProduct
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Category { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public  double GetdiscountedPrice()
+        {
+            return Price * 0.8;
+        }
+
+    }
+    class Default : IProduct
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Category { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public  double GetdiscountedPrice()
+        {
+            return Price;
+        }
+    }
+
+    class OrderProcessor
+    {
+        private readonly IDataBaseOperations _dbOperation;
+        private readonly ICommunication _communication;
+        public OrderProcessor(IDataBaseOperations dbOperation, ICommunication communication)
+        {
+            _dbOperation = dbOperation;
+            _communication = communication;
+        }
+
+        
+        
+
+        public void ProcessOrder(IProduct product)
+        {            
+            Console.WriteLine("processing order for: " + product.Name);
+            //DataBaseOperations dataBase = new DataBaseOperations();//DI
+            _dbOperation.SavetoDatabase(product);
+           // EmailCommunication emailCommunication = new EmailCommunication(); //DI
+            _communication.SendNotification(product);
+        }
+
+    }
+
+    interface IDataBaseOperations
+    {
+        void SavetoDatabase(IProduct product);
+    }
+    class DataBaseOperations: IDataBaseOperations
+    {
+        public void SavetoDatabase(IProduct product)
+        {
+            Console.WriteLine("processing order for: " + product.Name);
+        }
+
+    }
+
+    interface ICommunication
+    {
+        void SendNotification(IProduct product);
+    }
+    class EmailCommunication: ICommunication
+    {
+        public void SendNotification(IProduct product)
+        {
+            Console.WriteLine("processing order for: " + product.Name);
+        }
+
+    }
+
+
+
+    internal class SolidImplementation
+    {
+        static void Main()
+        {
+            IProduct electronic = new Electronics()
+            {
+                Name = "Test",
+                Category = "Test",
+                Color = "Red",
+                Price = 123
+            };
+            
+            IProduct sim = new SIM()
+            {
+                Name = "Test",
+                Category = "Test",
+                //Color = "Red",//vioalation of LSP
+                Price = 123
+            };
+
+            OrderProcessor orderProcessor = new OrderProcessor(new DataBaseOperations(), new EmailCommunication());
+            orderProcessor.ProcessOrder(electronic);
+            orderProcessor.ProcessOrder(sim);
+            
+        
+        }
+    }
+}
